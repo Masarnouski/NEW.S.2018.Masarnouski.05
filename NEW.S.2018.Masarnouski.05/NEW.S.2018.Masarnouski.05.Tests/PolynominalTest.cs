9cb21dd54ca3787762df623f7a1fab973e71490e
Евгений-ПК\Евгений <jeniamasarn94@gmail.com>
2018-03-27 23:52:45 +0300

    +add files

---
+++ NEW.S.2018.Masarnouski.05/NEW.S.2018.Masarnouski.05.Tests/PolynominalTest.cs
@@ -0,0 +1,125 @@
+using NUnit.Framework;
+using System;
+
+namespace NEW.S._2018.Masarnouski._05.Tests
+{
+    [TestFixture]
+    public class PolynominalTest
+    {
+        #region EqualsOperator
+        [Test]
+        [TestCase(new[] { 0d, 1d, 2d, 3d }, new[] { 1d, 1d }, ExpectedResult = false)]
+        [TestCase(new[] { 0d, 1d, 2d, 3d }, new[] { 1d, 1d, 1d, 1d }, ExpectedResult = false)]
+        public bool PolynomialOperatorEquals_DifferetLengthPolynomialToPolynomial_ReturnFalseTests(double[] coefficientsPolynomial1, double[] coefficientsPolynomial2)
+        {
+            Polynomial polynomial1 = new Polynomial(coefficientsPolynomial1);
+            Polynomial polynomial2 = new Polynomial(coefficientsPolynomial2);
+
+            return polynomial1 == polynomial2;
+        }
+        [Test]
+        [TestCase(new[] { 0d, 1d, 2d, 3d }, new[] { 0d, 1d, 2d, 3d }, ExpectedResult = true)]
+        [TestCase(new[] { 0d, 1d, 2d, 3d }, new[] { 3d, 2d, 1d, 0d }, ExpectedResult = false)]
+        public bool PolynomialOperator_Equals_Check_Arguments(double[] coefficientsPolynomial1, double[] coefficientsPolynomial2)
+        {
+            Polynomial polynomial1 = new Polynomial(coefficientsPolynomial1);
+            Polynomial polynomial2 = new Polynomial(coefficientsPolynomial2);
+
+            return polynomial1 == polynomial2;
+        }
+        [Test]
+        [TestCase(new[] { 0d, 1d, 2d, 3d })]
+        public void PolynomialOperator_Equals_When_Second_Argument_is_Null_ExceptionReturned(double[] coefficients)
+        {
+            Polynomial polynomial1 = new Polynomial(coefficients);
+            Polynomial polynomial2 = null;
+            bool b;
+            Assert.Throws<ArgumentNullException>(() => b = polynomial1 == polynomial2);
+        }
+        [Test]
+        [TestCase(new[] { 0d, 1d, 2d, 3d })]
+        public void PolynomialOperator_Equals_When_First_Argument_is_Null_ExceptionReturned(double[] coefficients)
+        {
+            Polynomial polynomial1 = null;
+            Polynomial polynomial2 = new Polynomial(coefficients);
+            bool b;
+            Assert.Throws<ArgumentNullException>(() => b = polynomial1 == polynomial2);
+        }
+        [Test]
+        [TestCase(null, null, ExpectedResult = true)]
+        public bool PolynomialOperator_Equals_NullArguments_FalseReturne(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            return polynomial1 == polynomial2;
+        }
+        #endregion
+        #region NotEqualOperators
+        [Test]
+        [TestCase(new[] { 0d, 1d, 2d, 3d }, new[] { 1d, 1d }, ExpectedResult = true)]
+        [TestCase(new[] { 0d, 1d, 2d, 3d }, new[] { 1d, 1d, 1d, 1d }, ExpectedResult = true)]
+        public bool PolynomialOperatorNotEqual_DifferetLengthPolynomialToPolynomial_ReturnTrueTests(double[] coefficientsPolynomial1, double[] coefficientsPolynomial2)
+        {
+            Polynomial polynomial1 = new Polynomial(coefficientsPolynomial1);
+            Polynomial polynomial2 = new Polynomial(coefficientsPolynomial2);
+
+            return polynomial1 != polynomial2;
+        }
+        [Test]
+        [TestCase(new[] { 0d, 1d, 2d, 3d }, new[] { 0d, 1d, 2d, 3d }, ExpectedResult = false)]
+        [TestCase(new[] { 0d, 1d, 2d, 3d }, new[] { 3d, 2d, 1d, 0d }, ExpectedResult = true)]
+        public bool PolynomialOperator_NotEquals_Check_Arguments(double[] coefficientsPolynomial1, double[] coefficientsPolynomial2)
+        {
+            Polynomial polynomial1 = new Polynomial(coefficientsPolynomial1);
+            Polynomial polynomial2 = new Polynomial(coefficientsPolynomial2);
+
+            return polynomial1 != polynomial2;
+        }
+        [Test]
+        [TestCase(new[] { 0d, 1d, 2d, 3d })]
+        public void PolynomialOperator_NotEquals_When_Second_Argument_is_Null_ExceptionReturned(double[] coefficients)
+        {
+            Polynomial polynomial1 = new Polynomial(coefficients);
+            Polynomial polynomial2 = null;
+            bool b;
+            Assert.Throws<ArgumentNullException>(() => b = polynomial1 != polynomial2);
+        }
+        [Test]
+        [TestCase(new[] { 0d, 1d, 2d, 3d })]
+        public void PolynomialOperator_NotEquals_When_First_Argument_is_Null_ExceptionReturned(double[] coefficients)
+        {
+            Polynomial polynomial1 = null;
+            Polynomial polynomial2 = new Polynomial(coefficients);
+            bool b;
+            Assert.Throws<ArgumentNullException>(() => b = polynomial1 != polynomial2);
+        }
+        [Test]
+        [TestCase(null, null, ExpectedResult = false)]
+        public bool PolynomialOperator_NotEquals_NullArguments_FalseReturne(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            return polynomial1 != polynomial2;
+        }
+        #endregion
+
+        
+        [TestCase(new double[0])]
+        public void Constructor_ArgumentExceptionReturned(double[] coefficients)
+        {
+            Assert.Throws<ArgumentException>(() => new Polynomial(coefficients));
+        }
+        
+        [TestCase(null)]
+        public void Constructor_ArgumentNullExceptionReturned(double[] coefficients)
+        {
+            Assert.Throws<ArgumentNullException>(() => new Polynomial(null));
+        }
+        
+        [TestCase(new double[] { 0, 1, 2, 3 }, ExpectedResult = "(0x^3) + (1x^2) + (2x^1) + (3)")]
+        [TestCase(new double[] { 0, 0, 0, 0 }, ExpectedResult = "(0x^3) + (0x^2) + (0x^1) + (0)")]
+        [TestCase(new double[] { 1.5, 0, -2, 3.78 }, ExpectedResult = "(1,5x^3) + (0x^2) + (-2x^1) + (3,78)")]
+        public string ToString_ConverDouble_ToPolynomialString(double[] coefficients)
+        {
+            Polynomial polynomial = new Polynomial(coefficients);
+
+            return polynomial.ToString();
+        }
+    }
+}
---
+++ NEW.S.2018.Masarnouski.05/NEW.S.2018.Masarnouski.05/Polynomial.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NEW.S._2018.Masarnouski._05
+{
+    public class Polynomial
+    {
+        #region fields
+        private double[] coefficients = new double[] { };
+
+        #endregion
+
+        #region constructor
+        public Polynomial(double[] coefficients)
+        {
+            if (coefficients is null)
+                throw new ArgumentNullException($"Argument {nameof(coefficients)} must be not null");
+            if (coefficients.Length == 0)
+                throw new ArgumentException($"Argument {nameof(coefficients)} length must be greater than zero ");
+            this.coefficients = coefficients;
+        }
+        #endregion
+
+        #region properties
+        public int Count
+        {
+            get { return coefficients.Length; }
+        }
+        public double this[int n]
+        {
+            get { return coefficients[n]; }
+            set { coefficients[n] = value; }
+        }
+        #endregion
+
+
+        #region overrided methods
+
+        public override string ToString()
+        {
+            string result = string.Empty;
+            for (int i = 0, j = this.Count - 1; i < this.Count; i++, j--)
+            {
+                if (i != coefficients.Length - 1)
+                {
+                    result += $"({coefficients[i]}x^{j}) + ";
+                }
+                else
+                {
+                    result += $"({coefficients[i]})";
+                }
+            }
+            return result;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is null && this is null)
+                return true;
+            if (obj is null || this is null)
+                return false;
+            
+            if (!(obj is Polynomial))
+                throw new ArgumentException($"Argument {nameof(obj)} must have a type of Polynomial");
+
+            if (this.Count != ((Polynomial)obj).Count)
+                return false;
+
+            Polynomial input = (Polynomial)obj;
+
+            bool isEqual = true;
+            for (int i = 0; i < this.Count && isEqual == true; i++)
+            {
+                if (input[i] == this[i])
+                    isEqual = true;
+                else
+                    isEqual = false;
+            }
+            return isEqual;
+        }
+        public override int GetHashCode()
+        {
+            int result = 0;
+
+            for (int i = 0, j = this.Count - 1; i < this.Count - 1; i++, j--)
+            {
+                result += (int)coefficients[i] ^ j;
+            }
+
+            return result.GetHashCode();
+        }
+
+        #endregion
+
+        #region operators method
+        /// <summary>
+        /// Compares two polynoms
+        /// </summary>
+        /// <param name="polynomial1"> The first polynom</param>
+        /// <param name="polynomial2"> The secind polynom </param>
+        /// <returns> return true if arguments is not equal </returns>
+        public static bool operator !=(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            if (polynomial1 is null && polynomial2 is null)
+                return false;
+            if (polynomial1 is null)
+                throw new ArgumentNullException($"Argument {nameof(polynomial1)} must be not null");
+            if (polynomial2 is null)
+                throw new ArgumentNullException($"Argument {nameof(polynomial2)} must be not null");
+            return !(polynomial1.Equals(polynomial2));
+        }
+
+        /// <summary>
+        /// Compares two polynomials
+        /// </summary>
+        /// <param name="polynomial1"> The first polynom</param>
+        /// <param name="polynomial2"> The secind polynom </param>
+        /// <returns> return true if arguments is equal </returns>
+        public static bool operator ==(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            if (polynomial1 is null && polynomial2 is null)
+                return true;
+            if (polynomial1 is null)
+                throw new ArgumentNullException($"Argument {nameof(polynomial1)} must be not null");
+            if (polynomial2 is null)
+                throw new ArgumentNullException($"Argument {nameof(polynomial2)} must be not null");
+            return polynomial1.Equals(polynomial2);
+        }
+
+        /// <summary>
+        ///adds two polynomials 
+        /// </summary>
+        /// <param name="polynomial1"> The first polynom</param>
+        /// <param name="polynomial2"> The secind polynom </param>
+        /// <returns> The polynom (sum of two polynomials) </returns>
+        public static Polynomial operator +(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            if (polynomial1 is null)
+                throw new ArgumentNullException($"Argument {nameof(polynomial1)} must be not null");
+            if (polynomial2 is null)
+                throw new ArgumentNullException($"Argument {nameof(polynomial2)} must be not null");
+
+            return Add(polynomial1, polynomial2);
+        }
+
+        /// <summary>
+        ///subtracts right polynomial from the left polynomial
+        /// </summary>
+        /// <param name="polynomial1"> The first polynom</param>
+        /// <param name="polynomial2"> The second polynom </param>
+        /// <returns> The polynom (substraction of two polynomials) </returns>
+        public static Polynomial operator -(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            if (polynomial1 is null)
+                throw new ArgumentNullException($"Argument {nameof(polynomial1)} must be not null");
+            if (polynomial2 is null)
+                throw new ArgumentNullException($"Argument {nameof(polynomial2)} must be not null");
+            return Substract(polynomial1, polynomial2);
+        }
+
+        /// <summary>
+        /// myltiply left and right polynoms
+        /// </summary>
+        /// <param name="polynomial1"> The first polynom </param>
+        /// <param name="polynomial2"> The second polynom </param>
+        /// <returns> The polynom (myltiplication of two polynomials) </returns>
+        public static Polynomial operator *(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            if (polynomial1 is null)
+                throw new ArgumentNullException($"Argument {nameof(polynomial1)} must be not null");
+            if (polynomial2 is null)
+                throw new ArgumentNullException($"Argument {nameof(polynomial2)} must be not null");
+
+            return Multiply(polynomial1, polynomial2);
+        }
+        #endregion
+
+
+        #region privatepart
+        private static Polynomial Add(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            double[] coeffPolynomial1 = new double[polynomial1.Count];
+            double[] coeffPolynomial2 = new double[polynomial2.Count];
+
+            coeffPolynomial1 = polynomial1.coefficients.Reverse().ToArray();
+            coeffPolynomial2 = polynomial2.coefficients.Reverse().ToArray();
+
+            int resultLength = Math.Max(coeffPolynomial1.Length, coeffPolynomial2.Length);
+            var result = new double[resultLength];
+
+            for (int i = 0; i < resultLength; i++)
+            {
+                double a = 0;
+                double b = 0;
+
+                if (i < coeffPolynomial1.Length)
+                {
+                    a = coeffPolynomial1[i];
+                }
+                if (i < coeffPolynomial2.Length)
+                {
+                    b = coeffPolynomial2[i];
+                }
+                result[i] = a + b;
+            }
+            return new Polynomial(result.Reverse().ToArray());
+        }
+
+        private static Polynomial Substract(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            double[] coeffPolynomial1 = new double[polynomial1.Count];
+            double[] coeffPolynomial2 = new double[polynomial2.Count];
+
+            coeffPolynomial1 = polynomial1.coefficients.Reverse().ToArray();
+            coeffPolynomial2 = polynomial2.coefficients.Reverse().ToArray();
+
+            int resultLength = Math.Max(coeffPolynomial1.Length, coeffPolynomial2.Length);
+            var result = new double[resultLength];
+
+            for (int i = 0; i < resultLength; i++)
+            {
+                double a = 0;
+                double b = 0;
+
+                if (i < coeffPolynomial1.Length)
+                {
+                    a = coeffPolynomial1[i];
+                }
+                if (i < coeffPolynomial2.Length)
+                {
+                    b = coeffPolynomial2[i];
+                }
+                result[i] = a - b;
+            }
+            return new Polynomial(result.Reverse().ToArray());
+        } 
+
+        private static Polynomial Multiply(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            int Count = polynomial1.Count + polynomial2.Count - 1;
+            var result = new double[Count];
+            for (int i = 0; i < polynomial1.Count; i++)
+            {
+                for (int j = 0; j < polynomial1.Count; j++)
+                {
+                    result[i + j] += polynomial1[i] * polynomial2[j];
+                }
+            }
+            return new Polynomial(result);
+        }
+        #endregion
+    }
+
+
+}
